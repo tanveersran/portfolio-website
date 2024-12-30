ca606e1b26504982d49d13fa42c7f596d1e48c09
Tanveer Sran <tanveersran07@gmail.com>
2024-12-29 20:09:40 -0500

    adjust section title font on large screens

--- my-portfolio/src/components/SectionTitle.js
+++ my-portfolio/src/components/SectionTitle.js
@@ -1,6 +1,6 @@
 const SectionTitle = ({ title }) => {
     return (
-        <span className="text-4xl text-primary-dark font-semibold lg:text-5xl">{title}</span>
+        <span className="text-4xl text-primary-dark font-semibold">{title}</span>
     );
 }
 
