fa86821421959c995fd5ca839e8f7a7226c7665e
Tanveer Sran <tanveersran07@gmail.com>
2024-12-29 19:20:49 -0500

    reduce circular image size for large screens

--- my-portfolio/src/components/CircularImage.js
+++ my-portfolio/src/components/CircularImage.js
@@ -2,18 +2,9 @@ import Image from "next/image";
 
 const CircularImage = ({ src, alt }) => {
     return (
-        <>  
-            {/* For desktop view*/}
-            <div className={`hidden lg:flex rounded-full overflow-hidden border-8 border-secondary-dark`}>
-                <Image src={src} alt={alt} width={300} height={300} className="object-cover" />
-            </div>
-        
-            {/* For mobile/tablet view */}
-            <div className={`flex lg:hidden rounded-full overflow-hidden border-8 border-secondary-dark`}>
+            <div className={`flex rounded-full overflow-hidden border-8 border-secondary-dark`}>
                 <Image src={src} alt={alt} width={200} height={200} className="object-cover" />
             </div>
-        </>
-
     );
 }
 
